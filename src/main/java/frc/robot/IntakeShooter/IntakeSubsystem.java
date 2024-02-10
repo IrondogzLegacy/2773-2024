c848fb89df488248ffd5296948e197eb7b2c8550
Space-Enthusiast-Nova <96958064+Space-Enthusiast-Nova@users.noreply.github.com>
2024-02-10 10:57:14 -0600

    updated imports

--- src/main/java/frc/robot/IntakeShooter/IntakeSubsystem.java
+++ src/main/java/frc/robot/IntakeShooter/IntakeSubsystem.java
@@ -4,11 +4,7 @@
 
 package frc.robot.IntakeShooter;
 
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants;
 
 import com.revrobotics.CANSparkMax;
