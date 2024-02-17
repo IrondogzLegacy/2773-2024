bd2c4199a8310ec25d320f42030e2e0fc1a89bb2
Space-Enthusiast-Nova <96958064+Space-Enthusiast-Nova@users.noreply.github.com>
2024-02-16 19:01:31 -0600

    added Camera code from 2023, updated to 2024 WPILIB

---
+++ src/main/java/frc/robot/CamSubsystem.java
@@ -0,0 +1,95 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class CamSubsystem extends SubsystemBase {
+  private final SerialPort serialPort = new SerialPort(115200, SerialPort.Port.kUSB);
+  /*
+   * Parses April Tag Data
+   * 
+   * Example April Tag data:
+   * Example simplified April Tag data:
+   * First: 1 0.328023 -0.520075 1.085208
+   * More data:
+   * First: 1 0.328262 -0.520233 1.085545
+   */
+
+  private TagData[] apriltag = new TagData[30];
+
+  public TagData apriltag(int i) {
+    return this.apriltag[i];
+  }
+  // Used for the data used in TurnToTagCommand.java
+
+  public static class TagData {
+    String apriltag;
+    double x; // How far right or left (I think)
+    double y; // How high or low the april tag is (mostly irrelavant for our purposes)
+    double z; // How far away (I think)
+    double alpha;
+  }
+
+  /*
+   * S = All data
+   * Tokens splits data based on ";",
+   * ids splits tokens based on ":"
+   * ids[0] = "TAG_FOUND", when april tag is found.
+   * ids[1] = the april tag ID, example: 1, 5, 16.
+   * xyz is tokens[2], it is the three floats at the end of String s
+   * When an april tag is found, a string will print with the id and xyz.
+   */
+  private static TagData parseTagData(String s) {
+    String[] tokens = s.split(";");
+    String[] ids = tokens[0].split(": ");
+    if (!ids[0].equals("TAG_FOUND") || tokens.length < 4) {
+      return null;
+    }
+
+    String apriltag = ids[1];
+
+    String Group1 = tokens[2];
+
+    String[] Num = Group1.split(",");
+
+    double XNum = Double.parseDouble(Num[0]);
+
+    double YNum = Double.parseDouble(Num[1]);
+
+    double ZNum = Double.parseDouble(Num[2]);
+
+    String TagMatrix = tokens[1];
+
+    String[] MatrixNum = TagMatrix.split(",");
+
+    double sinAlpha = Double.parseDouble(MatrixNum[0]);
+    double minusCosAlpha = Double.parseDouble(MatrixNum[2]);
+
+    TagData data = new TagData();
+    data.x = XNum;
+    data.y = YNum;
+    data.z = ZNum;
+    data.alpha = Math.atan2(sinAlpha,minusCosAlpha);
+    data.apriltag = apriltag;
+    return data;
+  }
+
+  // reads out what the epic camera saw
+  @Override
+  public void periodic() {
+    String s = serialPort.readString();
+    if (s != null) {
+      TagData tagData = parseTagData(s);
+      if (tagData != null) {
+        System.out.println("First: " + tagData.apriltag + " " + tagData.x + " " + tagData.y + " " + tagData.z);
+        // If there is data, then the data will be printed
+          this.apriltag[Integer.parseInt(tagData.apriltag)] = tagData;
+          // Used for the data used in TurnToTagCommand.java
+
+        
+      }
+    }
+
+    // This method will be called once per scheduler run
+  }
+}
---
+++ src/main/java/frc/robot/TurnToTagCommand.java
@@ -0,0 +1,109 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class TurnToTagCommand extends Command {
+  private final DriveSubsystem driveSubsystem;
+  private final CamSubsystem camSubsystem;
+  private NavigationSubsystem navigationSubsystem;
+  private final NetworkTable table = NetworkTableInstance.getDefault().getTable("April Tag");
+  private NetworkTableEntry tagIdEntry = table.getEntry("Id");
+
+  /* Creates a new TurnToTag. */
+  public TurnToTagCommand(DriveSubsystem driveSubsystem, CamSubsystem camSubsystem,
+      NavigationSubsystem navigationSubsystem) {
+    this.driveSubsystem = driveSubsystem;
+    this.camSubsystem = camSubsystem;
+    this.navigationSubsystem = navigationSubsystem;
+  }
+  double turnAngle;
+  double angleToTag;
+  double dis;
+  double z;
+  double x;
+  double sinAlpha;
+  double minusCosAlpha;
+  double tagRotation;
+  double fullTurnAngle;
+  double theta;
+  double distanceB;
+  double distanceA;
+  double conversionToDeg = 180./Math.PI;
+  double conversionToRad = Math.PI/180.;
+  double rotateSign;
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    long id = tagIdEntry.getInteger(1);
+        var tagData = camSubsystem.apriltag((int)id);
+    // angle = x < 0 ? -30 : 30;
+    // turnAngle = x;
+    if (tagData != null) { 
+    System.out.println(id);
+    x = tagData.x;
+    z = tagData.z;
+    tagRotation = tagData.alpha;
+    System.out.println("Alpha " + tagRotation);
+    if (tagRotation > 0) {fullTurnAngle = 90-tagRotation*conversionToDeg;}
+    if (tagRotation < 0) {fullTurnAngle = -90-tagRotation*conversionToDeg;}
+
+    if (fullTurnAngle > 0) {rotateSign = -90;}
+    if (fullTurnAngle < 0) {rotateSign = 90;}
+    System.out.println("Beta " + fullTurnAngle);
+    //turn the above angle
+    theta = (fullTurnAngle - angleToTag)*conversionToRad;
+    System.out.println("Theta " + theta);
+    distanceB = Math.cos(theta)*distanceToTag();
+    System.out.println("B " + distanceB);
+    //drive the above distance after turning
+    distanceA = Math.sin(theta)*distanceToTag();
+    System.out.println("A "+distanceA);
+    //drive the above distance minus some # after the previous drive finishes
+    System.out.println("Tag One Found");
+    /*RotationCommand rotationCommand = new RotationCommand(driveSubsystem, navigationSubsystem, fullTurnAngle);
+    MoveDistanceCommand moveDistanceB = new MoveDistanceCommand(driveSubsystem, navigationSubsystem, distanceB);
+    MoveDistanceCommand moveDistanceA = new MoveDistanceCommand(driveSubsystem, navigationSubsystem, distanceA-1);
+    RotationCommand rotate90 = new RotationCommand(driveSubsystem, navigationSubsystem, rotateSign);*/
+
+    //rotationCommand.andThen(moveDistanceB).andThen(rotate90).andThen(moveDistanceA).schedule();
+    //rotationCommand.schedule();
+  }
+}
+
+  public double distanceToTag() {
+    var distanceToTag = (Math.sqrt(x * x + z * z) * 3.28);
+    // For movement, the robot will turn angleToTag, and then move distanceToTag
+    System.out.println(x);
+    return distanceToTag;
+  }
+
+  public double angletoTag() {
+    angleToTag = Math.atan2(x, z) / Math.PI * 180;
+    System.out.println(angleToTag);
+    return angleToTag;
+
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
+}
