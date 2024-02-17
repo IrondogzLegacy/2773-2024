31622d9d7db61b1ee9626574d5edb8206ec42057
Space-Enthusiast-Nova <96958064+Space-Enthusiast-Nova@users.noreply.github.com>
2024-02-16 20:39:03 -0600

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -11,8 +11,8 @@ public final class Constants {
     public static final double HALF_WHEEL_DISTANCE = 0.5207;
     public static final MotorType motorType = MotorType.kBrushless;
     public static final int intakeMotorCANID = 18;
-    public static final int shooterMotor1CANID = 20;
-    public static final int shooterMotor2CANID = 24; 
+    public static final int shooterMotor1CANID = 15;
+    public static final int shooterMotor2CANID = 13; 
     public static final double intakeSpeed = 0.3;
     public static final double shooterSpeed = 0.3;
     
