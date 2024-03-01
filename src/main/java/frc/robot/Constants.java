a6ebaf750de439bdd949f1acbda05edb47581d22
Space-Enthusiast-Nova <96958064+Space-Enthusiast-Nova@users.noreply.github.com>
2024-02-29 18:56:58 -0600

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -57,7 +57,7 @@ public final class Constants {
     
     
     
-    public static final int IRSensorPort = 9;    
+    public static final int IRSensorPort = 9;
     
     //Offsets for GoToTagCommand
 }
